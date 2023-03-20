33c2c483c8f17dbb9c5eca5a5c086c2bb93c07b9
Anh Hoang <anhh34711@gmail.com>
2023-03-21 00:08:23 +0700

    update

--- app/src/main/java/com/example/project_prm392_se1614/dao/MyDatabase.java
+++ app/src/main/java/com/example/project_prm392_se1614/dao/MyDatabase.java
@@ -9,13 +9,16 @@ import androidx.room.TypeConverters;
 
 import com.example.project_prm392_se1614.converter.RoleConverter;
 import com.example.project_prm392_se1614.entity.Evaluate;
+import com.example.project_prm392_se1614.entity.FoodMaterial;
+import com.example.project_prm392_se1614.entity.Material;
+import com.example.project_prm392_se1614.entity.Review;
 import com.example.project_prm392_se1614.repository.EvaluateDao;
 import com.example.project_prm392_se1614.entity.Food;
 import com.example.project_prm392_se1614.repository.FoodDao;
 import com.example.project_prm392_se1614.entity.User;
 import com.example.project_prm392_se1614.repository.UserDao;
 
-@Database(entities = {User.class, Food.class, Evaluate.class}, version = 1)
+@Database(entities = {User.class, Food.class, Evaluate.class, Material.class, FoodMaterial.class, Review.class}, version = 1)
 @TypeConverters({RoleConverter.class})
 public abstract class MyDatabase extends RoomDatabase {
 
